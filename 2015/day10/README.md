d6a884fa958923d23b0fc223b5ea6458ed7ddeca
Kasey McKenna <kmckenna@zynga.com>
2020-12-11 23:19:09 -0800

    [2015 Day 10]

---
+++ 2015/day10/README.md
@@ -0,0 +1,12 @@
+# 2015 Day 10
+
+General Time Estimate
+- Part 1: O(n^(2 * 40)) - result^2 40 times? because worst case is no repeating digits?
+- Part 2: O(n^(2 * 50)) ?
+
+Benchmarks
+- Part 1: **292.34ms**
+- Part 2: **4,394ms**
+
+
+
---
+++ 2015/day10/part1.py
@@ -0,0 +1,31 @@
+from timeit import default_timer as timer
+
+iterations = 40
+
+def readline(line):
+	result = ""
+	prev = line[0]
+	count = 1
+	for i in range(1, len(line)):
+		char = line[i]
+		if char == prev:
+			count += 1
+		else:
+			result += str(count) + prev
+			count = 1
+			prev = char
+	result += str(count) + prev
+	return result
+
+start = timer()
+file = open('input.txt')
+
+line = file.readlines()[0].strip()
+
+for i in range(0, iterations):
+	line = readline(line)
+
+result = len(line)
+
+print("Completed in %fms" % ((timer() - start) * 1000))
+print("%d is the result" % result)
