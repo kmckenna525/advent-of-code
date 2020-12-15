4b3e80b2b252aae7719e077d9149d3bd262aa9b2
Kasey McKenna <kmckenna@zynga.com>
2020-12-14 22:16:08 -0800

    [2015 Day 13]

---
+++ 2015/day13/README.md
@@ -0,0 +1,13 @@
+# 2015 Day 13
+Copy pasta'd my traveling salesman and added back linking. Had trouble with the distance thing so made a path class instead of the add as you go thing I had for Day 9.
+
+General Time Estimate
+- Part 1: θ(n!) 
+- Part 2: θ(n!)
+
+Benchmarks
+- Part 1: **2,667.61ms**
+- Part 2: **29,423.87ms**
+
+
+
---
+++ 2015/day13/part2.py
@@ -0,0 +1,105 @@
+from timeit import default_timer as timer
+import re
+
+class Node:
+	def __init__(self, val):
+		self.val = val
+		self.neighbors = {}
+
+	def __str__(self):
+		return  "%s: %s" % (self.val, self.neighbors)
+
+	def __repr__(self):
+		return  self.val
+
+	def connect(self, node, length):
+		self.neighbors[node] = length
+
+class Path:
+	def __init__(self, existing):
+		self.path = existing
+
+	def __str__(self):
+		return  " -> ".join(self.path)
+
+	def __repr__(self):
+		return  " -> ".join(self.path)
+
+	def add(self, node):
+		return Path(self.path + [node])
+
+	def distance(self):
+		if len(self.path) <= 1:
+			return 0
+
+		last = self.path[-1] 
+		distance = 0
+		for i in range(0, len(self.path)):
+			node = self.path[i]
+			distance += last.neighbors[node]
+			distance += node.neighbors[last]
+			last = node
+		return distance
+
+nodes = {}
+def get_node(name):
+	if name not in nodes:
+		nodes[name] = Node(name)
+	return nodes[name]
+
+
+def get_paths(node, seen):
+	paths = []
+	seen.add(node)
+	for (neighbor, distance) in node.neighbors.items():
+		net = distance + neighbor.neighbors[node] # add the backlink
+		if neighbor in seen:
+			continue
+		sub_paths = get_paths(neighbor, seen)
+		for path in sub_paths:
+			paths.append(path.add(node)) 
+	seen.remove(node)
+	if len(paths) == 0: # post base case lol
+		paths.append(Path([node]))
+	return paths
+
+start = timer()
+file = open('input.txt')
+
+# process the nodes into a graph
+pattern = r"(?P<a>\w+) would (?P<sign>gain|lose) (?P<distance>\d+) happiness units by sitting next to (?P<b>\w+)";
+for line in file.readlines():
+	matches = re.match(pattern, line)
+	data = matches.groupdict();
+	
+	a = data["a"]
+	b = data["b"]
+	distance = int(data["distance"])
+	distance = -distance if data["sign"] == "lose" else distance
+
+	get_node(a).connect(get_node(b), distance)
+
+# add my node
+mynode = get_node("Kasey")
+for (name, node) in nodes.items():
+	if name == "Kasey":
+		continue
+	node.connect(mynode, 0)
+	mynode.connect(node, 0)
+
+
+# search paths
+seen = set()
+longest = None
+for (name, node) in nodes.items():
+	paths = get_paths(node, seen)
+	for path in paths:
+		# print (path, distance)
+		distance = path.distance()
+		if longest is None or distance > longest:
+			longest = distance
+
+result = longest
+
+print("Completed in %fms" % ((timer() - start) * 1000))
+print("%d is the result" % result)
