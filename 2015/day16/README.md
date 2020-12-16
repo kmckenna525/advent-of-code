f7734c8d408fb82c67653f83b637443b23557e87
Kasey McKenna <kmckenna@zynga.com>
2020-12-15 23:02:35 -0800

    [2015 Day 16]

---
+++ 2015/day16/README.md
@@ -0,0 +1,13 @@
+# 2015 Day 16
+Easy enough!
+
+General Time Estimate
+- Part 1: θ(n) 
+- Part 2: θ(n)
+
+Benchmarks
+- Part 1: **3.239870ms**
+- Part 2: **3.272057ms**
+
+
+
---
+++ 2015/day16/part2.py
@@ -0,0 +1,42 @@
+from timeit import default_timer as timer
+import re
+
+def parse_info(line):
+	info = {}
+	for item in line.split(", "):
+		pair = item.split(": ")
+		info[pair[0]] = int(pair[1])
+	return info
+
+
+start = timer()
+file = open('input.txt')
+lines = file.readlines()
+
+target = parse_info(lines[0].strip())
+
+def match(other):
+	for (key, val) in other.items():
+		if key in ["cats", "trees"]:
+			if val <= target[key]:
+				return False
+		elif key in ["pomeranians", "goldfish"]:
+			if val >= target[key]:
+				return False
+		elif val != target[key]:
+			return False
+	return True
+
+pattern = r"Sue (?P<id>\d+): (?P<list>.+)";
+result = -1
+for line in lines[1:]:
+	matches = re.match(pattern, line.strip())
+	data = matches.groupdict();
+	sue = int(data["id"])
+	info = parse_info(data["list"])
+	if match(info):
+		# print info
+		result = sue
+
+print("Completed in %fms" % ((timer() - start) * 1000))
+print("%d is the result" % result)
