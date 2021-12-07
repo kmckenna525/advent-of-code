167db2117f60053a3005031f8eac7d7a66a52e35
Kasey McKenna <kmckenna@zynga.com>
2021-12-06 21:22:10 -0800

    [2021 day 7] this one was like... too easy

---
+++ 2021/day07/README.md
@@ -0,0 +1,17 @@
+# 2021 Day 7
+
+Part 1: okay so first guess is that the average position is going to be the closes to optimal? but brute forcing is provable
+yeah brute forced for the most part but optimized with an early out (saved 30 ms)
+
+Part 2: That's a dang dong binomial coefficient, bro
+
+General Time Estimate
+- Part 1: θ(n^2) 
+- Part 2: θ(n^2)
+
+Benchmarks
+- Part 1: **89.941025ms**
+- Part 2: **134.545088ms**
+
+
+
---
+++ 2021/day07/part2.py
@@ -0,0 +1,43 @@
+from timeit import default_timer as timer
+import re
+
+start = timer()
+file = open('input.txt')
+
+# horizontal positions
+# 1 fuel per any one step
+# find optimal
+
+def calculate_fuel(positions, target, best = None):
+	fuel = 0
+	for position in positions:
+		n = abs(position - target)
+		fuel += (n * (n + 1)) / 2
+		if best is not None and fuel > best:
+			return None
+	return fuel
+
+
+target_min = None
+target_max = None
+positions = []
+for pos in file.readlines()[0].split(","):
+	pos = int(pos)
+	positions.append(pos)
+	if target_min is None or pos < target_min:
+		target_min = pos
+	if target_max is None or pos > target_max:
+		target_max = pos
+
+
+result = None
+optimal_target = None
+for target in range(target_min, target_max):
+	fuel = calculate_fuel(positions, target, result)
+	if fuel is not None and (result is None or fuel < result):
+		optimal_target = target
+		result = fuel
+
+
+print("Completed in %fms" % ((timer() - start) * 1000))
+print("position %d with %d fuel is the result" % (optimal_target, result))
