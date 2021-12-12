b2f8379bf21a9a7126ebb994f2aee4a70531a095
Kasey McKenna <kmckenna525@gmail.com>
2021-12-12 01:03:29 -0800

    [2015 day 20] part 1

--- 2015/day20/README.md
+++ 2015/day20/README.md
@@ -1,11 +1,14 @@
 # 2015 Day 20
 
+Part 1: # of gifts is the sum of all possible unique factors (x10). 
+Part 2: Looks like raw simulation of visits is better (and actually yeah, unique factorization takes a long time)
+
 General Time Estimate
 - Part 1: θ(n) 
 - Part 2: θ(n)
 
 Benchmarks
-- Part 1: **xxxms**
+- Part 1: **22008.011103ms**
 - Part 2: **xxxms**
 
 
--- 2015/day20/part1.py
+++ 2015/day20/part1.py
@@ -1,17 +1,44 @@
 from timeit import default_timer as timer
 import re
+import math
 
 start = timer()
 file = open('input.txt')
 
-pattern = r"(?P<group>\w+)";
-result = 0
-for line in file.readlines():
-	matches = re.match(pattern, line)
-	data = matches.groupdict();
-	group = data["group"]
-	
-	result += 1
+target = int(file.read().strip())
+
+
+def isprime(number):
+	sqrt = math.sqrt(number)
+	for factor in range(2, int(sqrt)+1):
+		if (number % factor) == 0:
+			return False
+	return True
+
+
+def factorize(number):
+	sqrt = int(math.sqrt(number))
+	yield 1
+	yield number
+	for factor in range(2, sqrt+1):
+		if (number % factor) == 0:
+			yield factor
+			yield int(number/factor)
+
+
+def calculate(number):
+	total = 0
+	for i in factorize(number):
+		total += i
+	return total * 10
+
+
+result = -1
+for i in range(1,(target/10 - 1)):
+	# print "House %d got %d presents." % (i, calculate(i))
+	if calculate(i) >= target:
+		result = i
+		break
 
 print("Completed in %fms" % ((timer() - start) * 1000))
 print("%d is the result" % result)
