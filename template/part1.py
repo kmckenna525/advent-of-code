6007258430d7936ac0ae82d0ea193a84a746c20e
Kasey McKenna <kmckenna@zynga.com>
2022-12-13 22:05:08 -0800

    [tools] update templates for proper file management

--- template/part1.py
+++ template/part1.py
@@ -2,16 +2,16 @@ from timeit import default_timer as timer
 import re
 
 start = timer()
-file = open('input.txt')
 
 pattern = r"(?P<group>\w+)";
 result = 0
-for line in file.readlines():
-	matches = re.match(pattern, line)
-	data = matches.groupdict();
-	group = data["group"]
-	
-	result += 1
+with open('input.txt') as file:
+	for line in file.readlines():
+		matches = re.match(pattern, line)
+		data = matches.groupdict();
+		group = data["group"]
+		
+		result += 1
 
 print("Completed in %fms" % ((timer() - start) * 1000))
 print("%d is the result" % result)
--- template/part2.py
+++ template/part2.py
@@ -29,16 +29,16 @@ class Node:
 		return 
 
 start = timer()
-file = open('input.txt')
 
 pattern = r"(?P<group>\w+)";
 result = 0
-for line in file.readlines():
-	matches = re.match(pattern, line)
-	data = matches.groupdict();
-	group = data["group"]
-	
-	result += 1
+with open('input.txt') as file:
+	for line in file.readlines():
+		matches = re.match(pattern, line)
+		data = matches.groupdict();
+		group = data["group"]
+		
+		result += 1
 
 print("Completed in %fms" % ((timer() - start) * 1000))
 print("%d is the result" % result)
