02be34ea26b28ddbda85d8ec4e88a2433a8cd43f
Kasey McKenna <kmckenna@zynga.com>
2022-12-13 21:58:42 -0800

    [2022 Day 14]

---
+++ 2022/day14/README.md
@@ -0,0 +1,17 @@
+# 2022 Day 14
+
+Part 1: I spent a lot of time getting the debug printing juuuust right. And then remembered I'm trying to go fast lol. Otherwise pretty straightforward once I had the input all parsed in. I thought about using ray tracing/intersections and then realized that's majorly overdoing it. 
+
+Part 2: The debug printing helped me because it gave me the max y value to use. 
+
+#### General Time Estimate
+Skipping this tonight, I'm tired
+
+#### Benchmarks
+- Part 1: **120.961905ms**
+- Part 2: **1752.897024ms**
+
+#### Stats
+```
+ 14   00:45:14    3325      0   00:51:52    2869      0
+```
---
+++ 2022/day14/part1.py
@@ -0,0 +1,127 @@
+from timeit import default_timer as timer
+
+start = timer()
+
+fall_origin = (500,0)
+
+empty = "."
+sand_hole = "+"
+resting_sand = "o"
+rock = "#"
+
+
+class DefaultValueMap:
+	def __init__(self, default = None):
+		self.map = {}
+		self.default = default
+		self.min = None
+		self.max = None
+
+	def __str__(self):
+		return str(self.map)
+
+
+	def __repr__(self):
+		return str(self.map)
+
+
+	def __getitem__(self, key):
+		if key not in self.map:
+			return self.default
+		return self.map[key]
+
+
+	def __setitem__(self, key, value):
+		self.map[key] = value
+		self.update_limits(key)
+
+	def update_limits(self, key):
+		if self.min is None:
+			self.min = [key[0], key[1]]
+		else:
+			if key[0] < self.min[0]:
+				self.min[0] = key[0]
+			if key[1] < self.min[1]:
+				self.min[1] = key[1]
+
+		if self.max is None:
+			self.max = [key[0], key[1]]
+		else:
+			if key[0] > self.max[0]:
+				self.max[0] = key[0]
+			if key[1] > self.max[1]:
+				self.max[1] = key[1]
+
+	def pop(self, key):
+		return self.map.pop(key)
+
+
+def values(a, b, pos):
+	if a[pos] <= b[pos]:
+		return range(int(a[pos]), int(b[pos]) + 1)
+	return range(int(b[pos]), int(a[pos]) + 1)
+
+
+def fill_segment(cavemap, a, b):
+	for x in values(a, b, 0):
+		for y in values(a, b, 1):
+			cavemap[(x,y)] = rock
+
+def debug_map(cavemap):
+	for y in range(cavemap.min[1], cavemap.max[1] + 1):
+		s = ""
+		for x in range(cavemap.min[0], cavemap.max[0] + 1):
+			point = (x,y)
+			s += cavemap[(x,y)]
+		print s
+
+
+def get_next_loc(cavemap, loc):
+	(x,y) = loc
+	loc = (x, y + 1)
+	if cavemap[loc] == empty:
+		return loc
+	loc = (x - 1, y + 1)
+	if cavemap[loc] == empty:
+		return loc
+	loc = (x + 1, y + 1)
+	if cavemap[loc] == empty:
+		return loc
+	return None
+
+
+
+def drop_sand(cavemap): # drop until rest - None if infinite
+	loc = fall_origin
+	while True:
+		next_loc = get_next_loc(cavemap, loc)
+		if next_loc is None:
+			return loc
+		if next_loc[1] > cavemap.max[1]:
+			return None
+		loc = next_loc
+
+
+cavemap = DefaultValueMap(empty)
+cavemap[fall_origin] = sand_hole
+with open('input.txt') as file:
+	for line in file.readlines():
+		points = [i.split(",") for i in line.strip().split(" -> ")]
+		for i in range(len(points) - 1):
+			# print "segment", points[i], points[i+1]
+			fill_segment(cavemap, points[i], points[i+1])
+
+# debug_map(cavemap)
+
+result = 0
+while True:
+	loc = drop_sand(cavemap)
+	if loc is None:
+		break
+	cavemap[loc] = resting_sand
+	result += 1	
+
+# debug_map(cavemap)
+
+print("Completed in %fms" % ((timer() - start) * 1000))
+print("%d is the result" % result)
